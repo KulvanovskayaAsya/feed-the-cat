7799f1c6e1b948ccfd0164c7953cb414d20c902f
unreal-zorro <unrealzorro@gmail.com>
2024-05-21 23:51:58 +0300

    add GameObject class

---
+++ packages/client/src/components/Game/classes/GameObject.ts
@@ -0,0 +1,43 @@
+export type GameObjectType = 'Hero' | 'Enemy' | 'Eat' | ''
+
+export interface GameObjectProps {
+  x: number
+  y: number
+}
+
+export class GameObject {
+  x: number
+  y: number
+  type: GameObjectType = ''
+  dead = false
+  width = 0
+  height = 0
+  img: HTMLImageElement | undefined = undefined
+
+  constructor(props: GameObjectProps) {
+    const { x, y } = props
+
+    this.x = x
+    this.y = y
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    if (this.img) {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+  }
+
+  rectFromGameObject(): {
+    top: number
+    left: number
+    bottom: number
+    right: number
+  } {
+    return {
+      top: this.y,
+      left: this.x,
+      bottom: this.y + this.height,
+      right: this.x + this.width,
+    }
+  }
+}
