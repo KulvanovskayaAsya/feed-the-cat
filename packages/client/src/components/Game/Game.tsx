ba27e79e0c7ef297b479dd0cd8615b959ea23feb
unreal-zorro <unrealzorro@gmail.com>
2024-05-23 23:22:52 +0300

    add hero to map boundary collisions

--- packages/client/src/components/Game/Game.tsx
+++ packages/client/src/components/Game/Game.tsx
@@ -7,10 +7,43 @@ import {
   useState,
 } from 'react'
 import './Game.css'
-import { clearGame, loadTexture } from './utils'
-import { Sprite } from './classes'
+import { clearGame, loadTexture, rectangularCollision } from './utils'
+import { collisions } from './data'
+import { hero } from './data'
+import { Boundary, Sprite } from './classes'
 
-// Функция-обработчика нажатия клавиш
+const collisionsMap: number[][] = []
+const heroMap: number[][] = []
+
+// Размер карты 20*15 тайлов, 1 тайл 40*40 пикселей
+for (let i = 0; i < collisions.length; i += 20) {
+  collisionsMap.push(collisions.slice(i, 20 + i))
+}
+
+for (let i = 0; i < collisions.length; i += 20) {
+  heroMap.push(hero.slice(i, 20 + i))
+}
+
+const boundaries: Boundary[] = []
+const boundaryWidth = 40
+const boundaryHeight = 40
+
+collisionsMap.forEach((row: number[], i: number) => {
+  row.forEach((symbol: number, j: number) => {
+    if (symbol === 1034) {
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * boundaryWidth,
+            y: i * boundaryHeight,
+          },
+        })
+      )
+    }
+  })
+})
+
+// Функция-обработчик нажатия клавиш
 const onKeyDown = (
   event: KeyboardEvent,
   setPressedKey: Dispatch<SetStateAction<string>>,
@@ -40,7 +73,7 @@ const onKeyDown = (
   }
 }
 
-// Функция-обработчика отпускания клавиш
+// Функция-обработчик отпускания клавиш
 const onKeyUp = (
   event: KeyboardEvent,
   setPressedKey: Dispatch<SetStateAction<string>>
@@ -65,49 +98,6 @@ const onKeyUp = (
   }
 }
 
-// Функция для отрисовки изображений при загрузке игры
-const run = async (
-  canvas: HTMLCanvasElement,
-  ctx: CanvasRenderingContext2D | null,
-  setLevel: Dispatch<SetStateAction<Sprite | null>>,
-  setHero: Dispatch<SetStateAction<Sprite | null>>
-): Promise<void> => {
-  const levelImg = await loadTexture('level.png')
-  const heroImg = await loadTexture('heroDown.png')
-
-  const level = new Sprite({
-    position: { x: 0, y: 0 },
-    velocity: 0,
-    image: levelImg,
-  })
-  setLevel(level)
-
-  const hero = new Sprite({
-    position: { x: 0, y: 0 },
-    velocity: 0,
-    image: heroImg,
-  })
-  setHero(hero)
-
-  if (ctx) {
-    // ctx.drawImage(levelImg, 0, 0, canvas.width, canvas.height)
-    // ctx.drawImage(
-    //   heroImg,
-    //   0,
-    //   0,
-    //   heroImg.width / 4,
-    //   heroImg.height,
-    //   heroImg.width / 8,
-    //   canvas.height / 2 + heroImg.height * 2,
-    //   heroImg.width / 4,
-    //   heroImg.height
-    // )
-
-    level.draw(ctx)
-    hero.draw(ctx)
-  }
-}
-
 export interface GameProps {
   width?: number
   height?: number
@@ -126,57 +116,206 @@ export const Game: FC<GameProps> = (props: GameProps) => {
   const [level, setLevel] = useState<Sprite | null>(null)
   const [hero, setHero] = useState<Sprite | null>(null)
 
+  const [moving, setMoving] = useState<boolean>(true)
+
   // Эффект для обновления игры с частотой около 60 кадров в секунду
   useEffect(() => {
-    if (ctx) {
-      let animationFrameId = 0
-      let lastFrame = performance.now()
-      const alpha = 0.1
+    let animationFrameId = 0
+    let lastFrame = performance.now()
+    const alpha = 0.1
 
-      const render = (timestamp: number) => {
-        animationFrameId = window.requestAnimationFrame(render)
+    const render = (timestamp: number) => {
+      animationFrameId = window.requestAnimationFrame(render)
 
-        if (timestamp < lastFrame + 1000 / 60) {
-          return
-        }
+      if (timestamp < lastFrame + 1000 / 60) {
+        return
+      }
 
-        const canvas: HTMLCanvasElement | null = canvasRef.current
+      const canvas: HTMLCanvasElement | null = canvasRef.current
 
-        if (canvas) {
-          clearGame(canvas, ctx)
-        }
+      if (canvas) {
+        clearGame(canvas, ctx)
+      }
 
+      if (ctx) {
         if (level) {
           level.draw(ctx)
         }
 
         if (hero) {
+          if (pressedKey === 'ArrowUp' && lastKey === 'ArrowUp') {
+            for (let i = 0; i < boundaries.length; i++) {
+              const boundary = boundaries[i]
+
+              if (
+                rectangularCollision(
+                  hero,
+                  new Boundary({
+                    position: {
+                      x: boundary.position.x,
+                      y: boundary.position.y + 3,
+                    },
+                  })
+                )
+              ) {
+                console.log('colliding')
+                setMoving(false)
+                break
+              } else {
+                setMoving(true)
+              }
+            }
+
+            if (moving) {
+              hero.position.y -= hero.velocity
+            }
+          } else if (pressedKey === 'ArrowDown' && lastKey === 'ArrowDown') {
+            for (let i = 0; i < boundaries.length; i++) {
+              const boundary = boundaries[i]
+
+              if (
+                rectangularCollision(
+                  hero,
+                  new Boundary({
+                    position: {
+                      x: boundary.position.x,
+                      y: boundary.position.y - 3,
+                    },
+                  })
+                )
+              ) {
+                console.log('colliding')
+                setMoving(false)
+                break
+              } else {
+                setMoving(true)
+              }
+            }
+
+            if (moving) {
+              hero.position.y += hero.velocity
+            }
+          } else if (pressedKey === 'ArrowLeft' && lastKey === 'ArrowLeft') {
+            for (let i = 0; i < boundaries.length; i++) {
+              const boundary = boundaries[i]
+
+              if (
+                rectangularCollision(
+                  hero,
+                  new Boundary({
+                    position: {
+                      x: boundary.position.x + 3,
+                      y: boundary.position.y,
+                    },
+                  })
+                ) ||
+                hero.position.x < 3
+              ) {
+                console.log('colliding')
+                setMoving(false)
+                break
+              } else {
+                setMoving(true)
+              }
+            }
+
+            if (moving) {
+              hero.position.x -= hero.velocity
+            }
+          } else if (pressedKey === 'ArrowRight' && lastKey === 'ArrowRight') {
+            for (let i = 0; i < boundaries.length; i++) {
+              const boundary = boundaries[i]
+
+              if (
+                rectangularCollision(
+                  hero,
+                  new Boundary({
+                    position: {
+                      x: boundary.position.x - 3,
+                      y: boundary.position.y,
+                    },
+                  })
+                )
+              ) {
+                console.log('colliding')
+                setMoving(false)
+                break
+              } else {
+                setMoving(true)
+              }
+            }
+
+            if (moving) {
+              hero.position.x += hero.velocity
+            }
+          }
+
           hero.draw(ctx)
         }
 
-        const dt = (timestamp - lastFrame) / 1000
-        lastFrame = timestamp
-        const newFPS = FPS + (1 - alpha) * (1 / dt - FPS)
-
-        setFPS(newFPS)
+        boundaries.forEach((boundary: Boundary) => {
+          boundary.draw(ctx)
+        })
       }
 
-      render(0)
+      const dt = (timestamp - lastFrame) / 1000
+      lastFrame = timestamp
+      const newFPS = FPS + (1 - alpha) * (1 / dt - FPS)
 
-      return () => {
-        window.cancelAnimationFrame(animationFrameId)
-      }
+      setFPS(newFPS)
     }
-  }, [hero])
+
+    render(0)
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [hero, pressedKey, lastKey, moving, setMoving])
 
   // Эффект для начальной инициализации игры
   useEffect(() => {
+    const run = async (): Promise<void> => {
+      const levelImg = await loadTexture('level.png')
+      const heroImg = await loadTexture('heroDown.png')
+
+      const level = new Sprite({
+        position: { x: 0, y: 0 },
+        velocity: 0,
+        image: levelImg,
+      })
+      setLevel(level)
+
+      const heroArray: Sprite[] = []
+
+      heroMap.forEach((row: number[], i: number) => {
+        row.forEach((symbol: number, j: number) => {
+          if (symbol === 1035) {
+            heroArray.push(
+              new Sprite({
+                position: {
+                  x:
+                    j * boundaryWidth + (boundaryWidth - heroImg.width / 4) / 2,
+                  y: i * boundaryHeight + (boundaryHeight - heroImg.height) / 2,
+                },
+                velocity: 1,
+                image: heroImg,
+                frames: { max: 4 },
+              })
+            )
+          }
+        })
+      })
+
+      const heroSprite = heroArray[0]
+      setHero(heroSprite)
+    }
+
     const canvas: HTMLCanvasElement | null = canvasRef.current
 
     if (canvas) {
-      setCtx(canvas && canvas.getContext('2d'))
+      setCtx(canvas.getContext('2d'))
       clearGame(canvas, ctx)
-      run(canvas, ctx, setLevel, setHero).then()
+      run().then()
     }
   }, [ctx])
 
@@ -198,21 +337,6 @@ export const Game: FC<GameProps> = (props: GameProps) => {
       window.removeEventListener('keyup', keyUpEventHandler)
     }
   }, [])
-
-  // Эффект для обработки нажатий клавиш
-  useEffect(() => {
-    if (hero) {
-      if (pressedKey === 'ArrowUp' && lastKey === 'ArrowUp') {
-        hero.position.y -= 3
-      } else if (pressedKey === 'ArrowDown' && lastKey === 'ArrowDown') {
-        hero.position.y += 3
-      } else if (pressedKey === 'ArrowLeft' && lastKey === 'ArrowLeft') {
-        hero.position.x -= 3
-      } else if (pressedKey === 'ArrowRight' && lastKey === 'ArrowRight') {
-        hero.position.x += 3
-      }
-    }
-  }, [hero, pressedKey, lastKey])
 
   return (
     <>
