1e6c7c41d391d959245a938888661673cab6ddcd
unreal-zorro <unrealzorro@gmail.com>
2024-06-02 22:36:03 +0300

    add css modules in pages

--- packages/client/src/pages/Error404/Error404Page.tsx
+++ packages/client/src/pages/Error404/Error404Page.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
 import { Flex } from 'antd'
-import './Error404Page.css'
+import cls from './Error404Page.module.css'
 import { Page } from '../Page'
 import { PixelButton, PixelCard, PixelHeader } from '../../components'
 import smallCat from '../../assets/smallCat.png'
@@ -9,19 +9,18 @@ export const Error404Page: FC = () => {
   return (
     <Page>
       <Flex align="center" justify="center" vertical>
-        <header className="error404-page-header">
+        <header className={cls.header}>
           <PixelHeader>
-            FEED THE{' '}
-            <img className="error404-page-img" src={smallCat} alt="cat" /> CAT
+            FEED THE <img className={cls.img} src={smallCat} alt="cat" /> CAT
           </PixelHeader>
         </header>
 
         <main>
           <Flex align="center" justify="center" vertical>
-            <PixelCard className="error404-page-card">
+            <PixelCard className={cls.card}>
               <Flex align="center" justify="flex-start" vertical>
-                <h2 className="error404-page-h2">OOPS</h2>
-                <p className="error404-page-text">Sorry, the page not found</p>
+                <h2 className={cls.h2}>OOPS</h2>
+                <p className={cls.text}>Sorry, the page not found</p>
               </Flex>
             </PixelCard>
 
