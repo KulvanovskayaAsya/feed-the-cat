58f60cfa33419486aa79e63c90da0c03d0f29ef7
unreal-zorro <unrealzorro@gmail.com>
2024-06-02 23:39:36 +0300

    add click handler in App.tsx

--- packages/client/src/pages/Error404/Error404Page.tsx
+++ packages/client/src/pages/Error404/Error404Page.tsx
@@ -1,33 +1,34 @@
 import type { FC } from 'react'
 import { Flex } from 'antd'
 import cls from './Error404Page.module.css'
-import { Page } from '@/pages'
 import { PixelButton, PixelCard, PixelHeader } from '@/components'
 import smallCat from '@/assets/smallCat.png'
+import { useNavigate } from 'react-router-dom'
 
 export const Error404Page: FC = () => {
+  const navigate = useNavigate()
+  const handleClick = () => navigate('/')
+
   return (
-    <Page>
-      <Flex align="center" justify="center" vertical>
-        <header className={cls.header}>
-          <PixelHeader>
-            FEED THE <img className={cls.img} src={smallCat} alt="cat" /> CAT
-          </PixelHeader>
-        </header>
+    <Flex align="center" justify="center" vertical>
+      <header className={cls.header}>
+        <PixelHeader>
+          FEED THE <img className={cls.image} src={smallCat} alt="cat" /> CAT
+        </PixelHeader>
+      </header>
 
-        <main>
-          <Flex align="center" justify="center" vertical>
-            <PixelCard className={cls.card}>
-              <Flex align="center" justify="flex-start" vertical>
-                <h2 className={cls.h2}>OOPS</h2>
-                <p className={cls.text}>Sorry, the page not found</p>
-              </Flex>
-            </PixelCard>
+      <main>
+        <Flex align="center" justify="center" vertical>
+          <PixelCard className={cls.card}>
+            <Flex align="center" justify="flex-start" vertical>
+              <h2 className={cls.h2}>OOPS</h2>
+              <p className={cls.text}>Sorry, the page not found</p>
+            </Flex>
+          </PixelCard>
 
-            <PixelButton>MAIN MENU</PixelButton>
-          </Flex>
-        </main>
-      </Flex>
-    </Page>
+          <PixelButton onClick={handleClick}>MAIN MENU</PixelButton>
+        </Flex>
+      </main>
+    </Flex>
   )
 }
