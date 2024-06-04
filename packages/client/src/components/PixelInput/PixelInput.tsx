4f2b5fdac0422109701efb66b9d88a80d9b5982f
Кузьменко Андрей Сергеевич <Kuzmenko.A.Sergee@sberbank.ru>
2024-06-04 14:28:40 +0300

    Добавил экран создания топика

--- packages/client/src/components/PixelInput/PixelInput.tsx
+++ packages/client/src/components/PixelInput/PixelInput.tsx
@@ -2,16 +2,20 @@ import { Input, InputProps } from 'antd'
 import { PixelBorder } from '../PixelBorder'
 import { PixelBorderProps } from '../PixelBorder/PixelBorder'
 import styles from './PixelInput.module.css'
+import { classNames } from '@/utils'
 
 type PixelInputProps = {
   borderProps?: PixelBorderProps
 }
 
 export const PixelInput = (props: PixelInputProps & InputProps) => {
+  const { className, borderProps, ...rest } = props
   return (
     <div className={styles['input-wrapper']}>
-      <PixelBorder {...props.borderProps} />
-      <Input className={styles['input']} {...props}></Input>
+      <PixelBorder {...borderProps} />
+      <Input
+        className={classNames(styles['input'], {}, [className])}
+        {...rest}></Input>
     </div>
   )
 }
