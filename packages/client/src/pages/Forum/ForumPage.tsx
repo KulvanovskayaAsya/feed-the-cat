eb56287332f3713b510a8e535d048c6929cc47ca
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-06-04 18:23:23 +0300

    Squashed commit of the following:
    commit 4f2b5fdac0422109701efb66b9d88a80d9b5982f
    Author: Кузьменко Андрей Сергеевич <Kuzmenko.A.Sergee@sberbank.ru>
    Date:   Tue Jun 4 14:28:40 2024 +0300
    
        Добавил экран создания топика
    
    commit b336f79c75e28195672c731a6eced859824aa096
    Author: Кузьменко Андрей Сергеевич <Kuzmenko.A.Sergee@sberbank.ru>
    Date:   Tue Jun 4 14:02:23 2024 +0300
    
        Добавил экран топика с комментариями
    
    commit 6c8aa01dae3bb22cb4cf0e972434818418582b5b
    Author: Кузьменко Андрей Сергеевич <Kuzmenko.A.Sergee@sberbank.ru>
    Date:   Tue Jun 4 03:35:55 2024 +0300
    
        Добавил верстку страницы с топиками

--- packages/client/src/pages/Forum/ForumPage.tsx
+++ packages/client/src/pages/Forum/ForumPage.tsx
@@ -1,5 +1,59 @@
+import { Flex } from 'antd'
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { PixelButton, PixelCard, PixelHeader } from '@/components'
+import smallCat from '@/assets/smallCat.png'
+import styles from './ForumPage.module.css'
+
+const fakeTopics = Array.from({ length: 5 }, (_, i) => ({
+  id: i,
+  name: 'Level ' + (i + 1),
+  messageCount: Math.ceil(Math.random() * 10),
+}))
 
 export const ForumPage: FC = () => {
-  return <h1>Forum Page</h1>
+  const navigate = useNavigate()
+  const handleCreate = () => navigate('/forum/topic/create')
+  const handleSelect = (id: number) => navigate('/forum/topic/' + id)
+
+  return (
+    <Flex
+      vertical
+      justify="space-around"
+      align="center"
+      gap={16}
+      className={styles.page}>
+      <header>
+        <PixelHeader>
+          FEED THE <img src={smallCat} alt="cat" /> CAT
+        </PixelHeader>
+      </header>
+      <main>
+        <Flex vertical gap={16}>
+          <Flex justify="space-between" className={styles['header-row']}>
+            <div>Name</div>
+            <div>Messages</div>
+          </Flex>
+          {fakeTopics.map(topic => {
+            return (
+              <div
+                key={String(topic.id)}
+                onClick={() => handleSelect(topic.id)}
+                className={styles['topic-row-wrapper']}>
+                <PixelCard>
+                  <Flex justify="space-between" className={styles['topic-row']}>
+                    <div>{topic.name}</div>
+                    <div>{topic.messageCount}</div>
+                  </Flex>
+                </PixelCard>
+              </div>
+            )
+          })}
+        </Flex>
+      </main>
+      <footer>
+        <PixelButton onClick={handleCreate}>Create new topic</PixelButton>
+      </footer>
+    </Flex>
+  )
 }
--- packages/client/src/pages/ForumTopicCreation/ForumTopicCreationPage.tsx
+++ packages/client/src/pages/ForumTopicCreation/ForumTopicCreationPage.tsx
@@ -1,5 +1,44 @@
-import { FC } from 'react'
+import { Flex } from 'antd'
+import { FC, useState } from 'react'
+import { PixelButton, PixelHeader, PixelInput } from '@/components'
+import styles from './ForumTopicCreation.module.css'
+import tokens from '../../../tokens.json'
+import { useNavigate } from 'react-router-dom'
 
 export const ForumTopicCreationPage: FC = () => {
-  return <h1>Forum Topic Creation Page</h1>
+  const navigate = useNavigate()
+  const [topicName, setTopicName] = useState('')
+
+  const handleCreate = () => {
+    navigate('/forum')
+  }
+
+  return (
+    <Flex
+      vertical
+      justify="space-around"
+      align="center"
+      gap={16}
+      className={styles.page}>
+      <header>
+        <PixelHeader className={styles['topic-name']}>
+          CREATE NEW TOPIC
+        </PixelHeader>
+      </header>
+      <main>
+        <PixelInput
+          className={styles.input}
+          placeholder="Title"
+          value={topicName}
+          onChange={e => setTopicName(e.target.value)}
+          borderProps={{
+            color: tokens['color-accent-secondary'],
+          }}
+        />
+      </main>
+      <footer>
+        <PixelButton onClick={handleCreate}>CREATE</PixelButton>
+      </footer>
+    </Flex>
+  )
 }
