100775ef9d9f6baba4b8dc8a30cf93567bc0cd9e
anastasiarebr <anastasiarebr@gmail.com>
2024-06-12 02:56:12 +0300

    [SHAKE-38, SHAKE-39, SHAKE-48] home, start, end

--- packages/client/src/pages/Game/GamePage.tsx
+++ packages/client/src/pages/Game/GamePage.tsx
@@ -1,12 +1,17 @@
 import { FC, useEffect, useState } from 'react'
 import cls from './GamePage.module.css'
-import { Game, PixelHeader } from '@/components'
+import {
+  Game,
+  PixelHeader,
+  PixelSelect,
+  PixelButton,
+  PixelLink,
+} from '@/components'
 import smallCat from '@/assets/smallCat.png'
 import { initialGameData, useGameContext } from '@/context'
-import { Button, Card, Select } from 'antd'
+import { Space } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import { HEROES } from '@/components/Game/data'
-
 // Тип опции для компонента Select
 type OptionsType = { value: string; label: string }
 
@@ -19,6 +24,22 @@ const getOptions: () => OptionsType[] = () => {
   }
 
   return optionsArray
+}
+interface RulesProps {
+  isFullWidth?: boolean
+}
+const RulesComponent: FC<RulesProps> = ({ isFullWidth }: RulesProps) => {
+  return (
+    <div className={`${cls.rulesWrapper} ${isFullWidth ? cls.fullWidth : ''}`}>
+      <h2 className={cls.h2}>Rules of the game</h2>
+      <ul>
+        <li>1 Control the cat using the arrow buttons on your keyboard.</li>
+        <li>2 Find all the goodies during the game.</li>
+        <li>3 Avoid meeting enemies.</li>
+        <li>4 Good game!</li>
+      </ul>
+    </div>
+  )
 }
 
 export const GamePage: FC = () => {
@@ -58,14 +79,20 @@ export const GamePage: FC = () => {
       <main>
         {!isGameStart && !isGameFinish && (
           <>
-            <Select
-              className={cls.select}
-              defaultValue={String(heroVariant)}
-              onChange={(value: string) => setHeroVariant(Number(value))}
-              options={getOptions()}
-            />
+            <RulesComponent isFullWidth />
 
-            <Button onClick={() => setIsGameStart(true)}>Start game</Button>
+            <div className={cls.buttons}>
+              <PixelSelect
+                defaultValue={String(heroVariant)}
+                onChange={(value: string) => setHeroVariant(Number(value))}
+                options={getOptions()}
+              />
+
+              <PixelButton onClick={() => setIsGameStart(true)}>
+                START GAME
+              </PixelButton>
+              <PixelLink to="/">MAIN MENU</PixelLink>
+            </div>
           </>
         )}
 
@@ -74,47 +101,45 @@ export const GamePage: FC = () => {
         {isGameFinish && (
           <>
             {gameData.isWin && (
-              <h2 className={cls.h2}>Congratulations on your victory!</h2>
+              <h1 className={cls.h1}>CONGRATULATIONS ON YOUR VICTORY!</h1>
             )}
             {!gameData.isWin && (
-              <p className={cls.message}>Lose the game, don't be upset!</p>
+              <h1 className={cls.h1}>LOSE THE GAME, DON'T BE UPSET!</h1>
             )}
 
-            <Card className={cls.card}>
+            <div className={cls.space}>
               <h2 className={cls.h2}>Game results</h2>
-              <p>Scores: {gameData.scores}</p>
-              <p>Level: {gameData.level}</p>
-              <p>Time: {gameData.time}</p>
-              <p>Lives: {gameData.life}</p>
-            </Card>
 
-            <Button
-              onClick={() => {
-                setIsGameStart(false)
-                setIsGameFinish(false)
-              }}>
-              Play again
-            </Button>
+              <Space
+                direction="vertical"
+                size="small"
+                align={'start'}
+                className={cls.spaceScore}>
+                <p>Scores: {gameData.scores}</p>
+                <p>Level: {gameData.level}</p>
+                <p>Time: {gameData.time}</p>
+                <p>Lives: {gameData.life}</p>
+              </Space>
 
-            <Button onClick={() => navigate('/')}>Main menu</Button>
+              <div className={cls.buttons}>
+                <PixelButton
+                  onClick={() => {
+                    setIsGameStart(false)
+                    setIsGameFinish(false)
+                  }}>
+                  PLAY AGAIN
+                </PixelButton>
+
+                <PixelLink to="/">MAIN MENU</PixelLink>
+              </div>
+            </div>
           </>
         )}
       </main>
 
       {isGameStart && (
         <footer className={cls.footer}>
-          <div className={cls.footerWrapper}>
-            <h2 className={cls.h2}>Rules of the game</h2>
-
-            <ul>
-              <li>
-                1 Control the cat using the arrow buttons on your keyboard.
-              </li>
-              <li>2 Find all the goodies during the game.</li>
-              <li>3 Avoid meeting enemies.</li>
-              <li>4 Good game!</li>
-            </ul>
-          </div>
+          <RulesComponent />
         </footer>
       )}
     </>
--- packages/client/src/pages/Home/HomePage.tsx
+++ packages/client/src/pages/Home/HomePage.tsx
@@ -1,39 +1,46 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import styles from './HomePage.module.css'
+import { Space } from 'antd'
+import { PixelHeader } from '@/components'
+import smallCat from '@/assets/smallCat.png'
+import { PixelLink } from '@/components'
 
 export const HomePage: FC = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <section className={styles.homeWrapper}>
+        <header className={styles.homeHeader}>
+          <PixelHeader className={styles.homeH1}>
+            FEED THE <img className={styles.image} src={smallCat} alt="cat" />{' '}
+            CAT
+          </PixelHeader>
 
-      <nav style={{ background: 'white' }}>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/registration">Registration</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/game">Game</Link>
-          </li>
-          <li>
-            <Link to="/leaderboard">Leaderboard</Link>
-          </li>
-          <li>
-            <Link to="/forum">Forum</Link>
-          </li>
-          <li>
-            <Link to="/forum/topic/1">Forum Topic</Link>
-          </li>
-          <li>
-            <Link to="/forum/topic/create">Forum Topic Create</Link>
-          </li>
-        </ul>
-      </nav>
+          <p className={styles.homeSubtitle}>
+            This game is about a cat that you have to feed. You find yourself in
+            a room of a house where food is scattered. You need to collect all
+            the food within a certain time. But be careful! The robot vacuum
+            cleaner does not sleep. Better not meet him. Good luck in the
+            game!!!
+          </p>
+        </header>
+        <nav className={styles.homeNav}>
+          <ul>
+            <li>
+              <PixelLink to="/game">Game</PixelLink>
+            </li>
+            <li>
+              <PixelLink to="/profile">Profile</PixelLink>
+            </li>
+            <li>
+              <PixelLink to="/forum">Forum</PixelLink>
+            </li>
+            <li>
+              <PixelLink to="/leaderboard">Leaderboard</PixelLink>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </>
   )
 }
