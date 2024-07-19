269a1a5f3bca627fec546c1465c3a4f89220821d
Rebrova Anastasiya Dmitrievna <anastasiarebr@gmail.com>
2024-07-19 15:40:59 +0300

    [SHAKE-62] fix: test

--- packages/client/src/components/OAuthButton/OAuth.tsx
+++ packages/client/src/components/OAuthButton/OAuth.tsx
@@ -5,6 +5,7 @@ import { getServiceID } from '@/store/slices/serviceSlice'
 import { serviceSelectors } from '@/store/selectors'
 import { useAppDispatch } from '@/store'
 import { SERVICE_URL_LOCAL } from '@/api/urls'
+import { ThunkDispatch } from 'redux-thunk'
 
 import cls from './OAuth.module.css'
 
@@ -14,7 +15,7 @@ interface OAuthProps {
 
 export const OAuth: FC<OAuthProps> = ({ separatorText }) => {
   const service_id = useSelector(serviceSelectors.service_id)
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch<ThunkDispatch<any, any, any>>()
 
   const oauth = async () => {
     await dispatch(getServiceID({ redirect_uri: SERVICE_URL_LOCAL }))
