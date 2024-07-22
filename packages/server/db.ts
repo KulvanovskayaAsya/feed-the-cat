07bbff1361603becca9825b5f6e052ea9be4cbbe
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-07-23 00:16:52 +0300

    docker ready steady go

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -4,10 +4,11 @@ const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
   process.env
 
 export const createClientAndConnect = async (): Promise<Client | null> => {
+  console.log('user is ', POSTGRES_USER)
   try {
     const client = new Client({
       user: POSTGRES_USER,
-      host: 'localhost',
+      host: 'postgres',
       database: POSTGRES_DB,
       password: POSTGRES_PASSWORD,
       port: Number(POSTGRES_PORT),
