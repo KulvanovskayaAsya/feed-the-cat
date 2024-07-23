487b7048bb4b65f3b7277c6ac05dc665717cc495
Rebrova Anastasiya Dmitrievna <anastasiarebr@gmail.com>
2024-07-23 11:31:34 +0300

    [SHAKE-62] fix

--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,50 +1,24 @@
-import { Col, Flex, notification, Row, Spin } from 'antd'
-import { FC, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import styles from './Leaderboard.module.css'
+import { Flex } from 'antd'
+import { FC } from 'react'
 import { PixelCard, PixelHeader, PixelAvatar } from '@/components'
+import styles from './Leaderboard.module.css'
 import tokens from '../../../tokens.json'
 import { classNames } from '@/utils'
-import { useAppDispatch } from '@/store'
-import { leaderboardSelectors, userSelectors } from '@/store/selectors'
-import { get } from '@/store/slices/leaderboardSlice'
-import type { LeaderboardRequest } from '@/api/leaderboard-api'
-import { ThunkDispatch } from 'redux-thunk'
+
+const currentUser = {
+  id: 2,
+}
+
+const data = Array.from({ length: 5 }, (_, i) => ({
+  id: i,
+  avatar: '',
+  name: 'Name ' + Math.ceil(Math.random() * 5),
+  score: Math.ceil(Math.random() * 100) + 100 * (5 - i),
+}))
 
 const firstPlaceColor = '#F8D028'
 
 export const LeaderboardPage: FC = () => {
-  const [api, contextHolder] = notification.useNotification()
-  const dispatch = useAppDispatch<ThunkDispatch<any, any, any>>()
-
-  const data = useSelector(leaderboardSelectors.leaderboard)
-  const error = useSelector(leaderboardSelectors.error)
-  const isLoading = useSelector(leaderboardSelectors.isLoading)
-
-  const currentUser = useSelector(userSelectors.user)
-
-  useEffect(() => {
-    const getLeaderboardData = async (body: LeaderboardRequest) => {
-      await dispatch(get(body))
-    }
-
-    const body = {
-      ratingFieldName: 'scores',
-      cursor: 0,
-      limit: 100,
-    }
-
-    getLeaderboardData(body)
-  }, [])
-
-  useEffect(() => {
-    if (error) {
-      api.error({
-        message: 'Error receiving Leaderboard data',
-      })
-    }
-  }, [error])
-
   return (
     <Flex
       vertical
@@ -56,106 +30,31 @@ export const LeaderboardPage: FC = () => {
         <PixelHeader>LEADERBOARD</PixelHeader>
       </header>
       <main>
-        {contextHolder}
-
-        <Spin spinning={isLoading} fullscreen size={'large'} />
-
         <Flex vertical gap={32}>
-          {data.length === 0 && (
-            <PixelCard>
-              <Row justify="center" className={styles.row}>
-                <Col span={3} className={styles.col}>
-                  <div>No data</div>
-                </Col>
-              </Row>
-            </PixelCard>
-          )}
-
-          {data.length > 0 && (
-            <>
-              <PixelCard>
-                <Row justify="space-between" className={styles.row}>
-                  <Col span={3} className={styles.col}>
-                    <div>Avatar</div>
-                  </Col>
-
-                  <Col span={8} className={styles.col}>
-                    <div>Login</div>
-                  </Col>
-
-                  <Col span={2} className={styles.col}>
-                    <div>Level</div>
-                  </Col>
-
-                  <Col span={2} className={styles.col}>
-                    <div>Life</div>
-                  </Col>
-
-                  <Col span={2} className={styles.col}>
-                    <div>Time</div>
-                  </Col>
-
-                  <Col span={2} className={styles.col}>
-                    <div>Win</div>
-                  </Col>
-
-                  <Col span={2} className={styles.col}>
-                    <div>Scores</div>
-                  </Col>
-                </Row>
+          {data.map((row, rowIdx) => {
+            return (
+              <PixelCard
+                key={row.id}
+                className={classNames('', {
+                  [styles.currentUserRow]: row.id === currentUser.id,
+                })}>
+                <Flex gap={32} className={styles.row}>
+                  <PixelAvatar
+                    src={row.avatar}
+                    borderProps={{
+                      size: 5,
+                      color:
+                        rowIdx === 0
+                          ? firstPlaceColor
+                          : tokens['color-accent-secondary'],
+                    }}
+                  />
+                  <div>{row.name}</div>
+                  <div>{row.score}</div>
+                </Flex>
               </PixelCard>
-
-              {data.map((row, rowIdx) => {
-                return (
-                  <PixelCard
-                    key={row.data.id}
-                    className={classNames('', {
-                      [styles.currentUserRow]: row.data.id === currentUser.id,
-                    })}>
-                    <Row justify="space-between" className={styles.row}>
-                      <Col span={3} className={styles.colAvatar}>
-                        <PixelAvatar
-                          className={styles.rowAvatar}
-                          src={row.data.avatar}
-                          borderProps={{
-                            size: 5,
-                            color:
-                              rowIdx === 0
-                                ? firstPlaceColor
-                                : tokens['color-accent-secondary'],
-                          }}
-                        />
-                      </Col>
-
-                      <Col span={8} className={styles.col}>
-                        <div>{row.data.login}</div>
-                      </Col>
-
-                      <Col span={2} className={styles.col}>
-                        <div>{row.data.level}</div>
-                      </Col>
-
-                      <Col span={2} className={styles.col}>
-                        <div>{row.data.life}</div>
-                      </Col>
-
-                      <Col span={2} className={styles.col}>
-                        <div>{row.data.time}</div>
-                      </Col>
-
-                      <Col span={2} className={styles.col}>
-                        <div>{row.data.isWin ? 'yes' : 'no'}</div>
-                      </Col>
-
-                      <Col span={2} className={styles.col}>
-                        <div>{row.data.scores}</div>
-                      </Col>
-                    </Row>
-                  </PixelCard>
-                )
-              })}
-            </>
-          )}
+            )
+          })}
         </Flex>
       </main>
     </Flex>
--- packages/client/src/store/reducers/index.ts
+++ packages/client/src/store/reducers/index.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
 import { userReducer } from './user'
-import { leaderboardReducer } from './leaderboard'
 
 // В этом файле будем объединять все редьюсеры в один
 export default combineReducers({
   user: userReducer,
-  leaderboard: leaderboardReducer,
 })
