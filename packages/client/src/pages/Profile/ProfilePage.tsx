b1ab085ca2742b4392221405aeef7e1f3a29e392
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-08-13 19:50:54 +0300

    Update ProfilePage.tsx

--- packages/client/src/pages/Profile/ProfilePage.tsx
+++ packages/client/src/pages/Profile/ProfilePage.tsx
@@ -2,47 +2,29 @@ import React, { FC, useEffect } from 'react'
 import { useSelector } from '@/store'
 import { fetchUserThunk, selectUser } from '@/store/slices/userSlice'
 import { PageInitArgs } from '@/router/routes'
+
+import { Flex } from 'antd'
+import { Typography } from 'antd'
+
 import { usePage } from '@/utils/hooks'
+import { User } from '@/api/auth-api'
 
-async function testRequest() {
-  const uuid = '3d0abf17-31fc-4f76-b530-3d1987047604'
-  const url = `${process.env.EXTERNAL_SERVER_URL}/api/user`
-  const body = {
-    username: 'testuser',
-    password: 'testpassword',
-    email: 'testuser@example.com',
-  }
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: `uuid=${uuid}`,
-      },
-      body: JSON.stringify(body),
-    })
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-
-    const data = await response.json()
-    console.log('User created:', data)
-  } catch (error) {
-    console.error('Error creating user:', error)
-  }
-}
+const { Title } = Typography
 
 export const ProfilePage: FC = () => {
   const user = useSelector(selectUser)
+  const { first_name, second_name, display_name, email, phone } =
+    user as unknown as User
   usePage({ initPage: initProfilePage })
 
-  useEffect(() => {
-    testRequest()
-  }, [])
-
-  return <h1>{JSON.stringify(user)}</h1>
+  return (
+    <Flex>
+      <Title>Name: {`${first_name} ${second_name}`}</Title>
+      <Title level={2}>Username: {display_name}</Title>
+      <Title level={3}>Email: {email}</Title>
+      <Title level={3}>Phone: {phone}</Title>
+    </Flex>
+  )
 }
 
 export const initProfilePage = async ({ dispatch, state }: PageInitArgs) => {
