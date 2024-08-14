77fc877ce581c8c80637b68e29b4501c7cb85466
Anastasia Rebrova <anastasiarebr@gmail.com>
2024-08-14 14:21:33 +0300

    fixed Profile

--- packages/client/src/pages/Profile/ProfilePage.tsx
+++ packages/client/src/pages/Profile/ProfilePage.tsx
@@ -12,13 +12,12 @@ import { User } from '@/api/auth-api'
 const { Title } = Typography
 
 export const ProfilePage: FC = () => {
-  const user = useSelector(selectUser)
-  const { first_name, second_name, display_name, email, phone } =
-    user as unknown as User
+  const { user } = useSelector(selectUser)
+  const { first_name, second_name, display_name, email, phone } = user as User
   usePage({ initPage: initProfilePage })
 
   return (
-    <Flex>
+    <Flex vertical>
       <Title>Name: {`${first_name} ${second_name}`}</Title>
       <Title level={2}>Username: {display_name}</Title>
       <Title level={3}>Email: {email}</Title>
